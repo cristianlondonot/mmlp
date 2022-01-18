1413da07b3ec8a4e040d193e7211bbf4f6a43a3f
cristianlondonot <84142880+cristianlondonot@users.noreply.github.com>
2022-01-18 17:36:32 -0500

    Add files via upload

---
+++ chatconalma/js/main.js
@@ -0,0 +1,12 @@
+function init(){
+	var x = document.getElementById("aviso");
+    x.style.display = "none";
+}
+function myFunctionB1() {
+    var x = document.getElementById("aviso");
+    if (x.style.display === "none") {
+        x.style.display = "none";  
+    } else {
+        x.style.display = "none";      
+    }
+}
