e9f834537016e811fd75df227fc5647fc3d74225
cristianlondonot <84142880+cristianlondonot@users.noreply.github.com>
2022-01-18 14:29:41 -0500

    Add files via upload

---
+++ tarotdeangela/js/main.js
@@ -0,0 +1,24 @@
+function showInp(){
+    getSelectValue = document.getElementById("other-option").value;
+    if(getSelectValue=="value10"){
+      document.getElementById("other-option-ext").style.display = "inline-block";
+    }else if(getSelectValue=="value9"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value8"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value7"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value6"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value5"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value4"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value3"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value2"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }else if(getSelectValue=="value1"){
+      document.getElementById("other-option-ext").style.display = "none";
+    }
+  }
