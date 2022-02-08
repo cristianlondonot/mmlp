5f96c227909654dd9065e9cfd4481a6a759b7f34
Cristian Camilo Lodoño Taborda <84142880+cristianlondonot@users.noreply.github.com>
2022-02-08 18:53:54 -0500

    Add files via upload

---
+++ chatconalma/js/main.js
@@ -0,0 +1,16 @@
+function init(){
+	var x = document.getElementById("aviso");
+    x.style.display = "none";
+}
+function myFunctionB1() {
+    var x = document.getElementById("aviso");
+    if (x.style.display === "none") {
+        x.style.display = "none";  
+    } else {
+        x.style.display = "none";      
+    }
+}
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
