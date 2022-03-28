2e3f3411780158f5f004ab8feba49ecd80f0d9fc
Cristian Camilo Lodoño Taborda <84142880+cristianlondonot@users.noreply.github.com>
2022-03-27 20:53:57 -0500

    Add files via upload

---
+++ tarotmagicodeeva-agenda/js/main.js
@@ -0,0 +1,24 @@
+new Swiper('.swiper', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      }
+    }
+});
